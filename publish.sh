076ff3cd84b9ceea14b98c37e130f944c6806a7f
Adnan Kamili <adnan.kamili@gmail.com>
2019-07-09 13:08:10 +0530

    updated travis

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 python pre-publish.py
 python setup.py sdist bdist_wheel
-python -m pip install --user --upgrade twine
+# python -m pip install --user --upgrade twine
 python -m twine upload dist/*
 rm -rf ./dist
